8e0c7625c4196f969c51ed7ae4e2b707db602fbc
Fernanda da Paz <adnanrefzap@gmail.com>
2025-01-10 20:25:17 -0300

    savedados

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from classe import Matrix
 import numpy as np
 import random
-import math
+import csv
+
 """
     Realiza a distribuição das partículas em uma linha horizontal utilizando o conceito de voo de Lévy.
 
@@ -18,40 +20,49 @@ import math
     - Se o sítio estiver vazio, verifica-se se a partícula pode ocupá-lo.
     - A particula G só pode ser inserida em locais onde seus vizinhos são zeros ou particulas pequenas, nunca uma G pode estar ao lado de outra
     - Caso a partícula possa ser inserida, ela é posicionada na matriz. Caso contrário, o processo é reiniciado.
-    - toda vez que nós tentamos inserir uma particula, contabilizamos +1 em uma variavel para saber quantas tentativas ocorreram de inserir particulas
+    - Toda vez que tentamos inserir uma partícula, contabilizamos +1 em uma variável para saber quantas tentativas ocorreram de inserir partículas.
 """
-quantidade = 10 # quantidade de linhas da rede
+
+quantidade = 10  # quantidade de linhas da rede
 tempo = 5
 t_simulacao = quantidade * tempo
 
-sitios = Matrix(int(quantidade))  # objeto criado
+sitios = Matrix(int(quantidade))
 
-p_reacao= 1.0
+p_reacao = 1.0
 p_levy = 0.9  # probabilidade de ocorrer o voo de Lévy
-pps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-pp = 0.3
+pps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  
 sigma = 1
 
-    
-def mostras(p_reacao, p_levy, pps, pp, sigma, quantidade, tempo, t_simulacao):
+x = datetime.now().strftime("%m%d_%H%M%S")
+
+def mostras(p_reacao, p_levy, pp, sigma, quantidade, tempo, t_simulacao):
     sitios.tentativas_totais = 0
     contador = 0
-    for i in range(t_simulacao):
-        if random.random() < p_levy:
-            sitios.voo_levy(p_reacao, sigma)
-            sitios.inserir_particula(pp, p_reacao)
-        else:
-            sitios.inserir_particula(pp, p_reacao)
+    
+    with open(f"r{x}.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
+        if file.tell() == 0:
+            writer.writerow(["Iteracao", "Densidade Vazia", "Densidade P", "Densidade G", "pp"])
         
-        contador += 1
-        if contador == quantidade:
-            sitios.salvar_estado()
-            contador = 0  
-            
-            
-    print(f'Total de tentativas: {sitios.tentativas_totais}\n')
-    print(f'Densidade vazia: {sitios.densidade_historico}\n')
-    print(f'Densidade P: {sitios.d_his_P}\n')
-    print(f'Densidade G: {sitios.d_his_G}\n')
+        for i in range(t_simulacao):
+            if random.random() < p_levy:
+                sitios.voo_levy(p_reacao, sigma)
+                sitios.inserir_particula(pp, p_reacao)
+            else:
+                sitios.inserir_particula(pp, p_reacao)
 
-mostras(p_reacao, p_levy, pps, pp, sigma, quantidade, tempo, t_simulacao)
+            contador += 1
+            if contador == quantidade:
+                sitios.salvar_estado()
+                contador = 0
+                writer.writerow([int((i + 1) / quantidade), 
+                                 sitios.densidade_historico[-1], 
+                                 sitios.d_his_P[-1], 
+                                 sitios.d_his_G[-1], 
+                                 pp])
+                    
+        print(f'Fazendo pp={pp}. Total de tentativas: {sitios.tentativas_totais}')
+
+for pp in pps:
+    mostras(p_reacao, p_levy, pp, sigma, quantidade, tempo, t_simulacao)
